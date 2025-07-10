d4020180fc119b237003c53cbdd161d35d2d6575
GeraGlush <75035157+GeraGlush@users.noreply.github.com>
2025-07-10 10:29:48 +0300

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,8 @@
-import { migrate, seed } from "#postgres/knex.js";
+import { migrate } from "#postgres/knex.js";
 import { initJob as initSyncingTariffsJob } from "#jobs/syncTariffs.js";
 
 await migrate.latest();
-await seed.run();
-console.log("All migrations and seeds have been run");
+console.log("All migrations have been run");
 
 initSyncingTariffsJob();
 console.log("Job was inited");
